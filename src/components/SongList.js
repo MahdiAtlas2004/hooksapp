514bd7144684cc4b22dbcbd47ef0ae3a237f5c34
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-30 08:28:51 +0430

    adding useEffect to SongList component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 //using uuid for creating unique id for evey new song that we add to the webpage
 import { v4 as uuidv4 } from "uuid";
 import NewSongForm from "./NewSongForm";
@@ -9,10 +9,16 @@ const SongList = () => {
     { title: "memory gospel", id: 2 },
     { title: "this wild darkness", id: 3 },
   ]);
-
+  const [age, setAge] = useState(20);
   const addSong = (title) => {
     setSongs([...songs, { title, id: uuidv4() }]);
   };
+  useEffect(() => {
+    console.log("useEffect hook", songs);
+  }, [songs]);
+  useEffect(() => {
+    console.log("useEffect hook", age);
+  }, [age]);
   return (
     <div className="song-list">
       <ul>
@@ -21,6 +27,7 @@ const SongList = () => {
         })}
       </ul>
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add by 1: {age}</button>
     </div>
   );
 };
